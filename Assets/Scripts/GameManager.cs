32f0ed191719efa931f339091d76804960c65226
Ahmet Altunbaş <140913760+Ahmet272@users.noreply.github.com>
2024-07-10 15:13:45 +0300

    aa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [Header("------ARABA AYARLARI")]
     public GameObject[] Arabalar;
+    public GameObject[] ArabaCanvasGorselleri;
+    public Sprite AracGeldiGorseli;
     public int KacArabaOlsun;
     public GameObject DurusNoktasý;
     int AktifAracIndex = 0;
@@ -19,10 +22,10 @@ public class GameManager : MonoBehaviour
     public float[] DonusHizlari;
     void Start()
     {
-        /* for (int i = 0; i < KacArabaOlsun; i++)
+         for (int i = 0; i < KacArabaOlsun; i++)
          {
-
-         }*/
+            ArabaCanvasGorselleri[i].SetActive(true);
+         }
        
     }
 
@@ -34,6 +37,7 @@ public class GameManager : MonoBehaviour
            
             Arabalar[AktifAracIndex].SetActive(true);
         }
+        ArabaCanvasGorselleri[AktifAracIndex -1].GetComponent<Image>().sprite = AracGeldiGorseli;
     }
     void Update()
     {
